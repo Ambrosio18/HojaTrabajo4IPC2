18936ed1cd893532d4863a36b2bd51d861da08a1
Ambrosio18 <ferambrosio1809@gmail.com>
2017-07-03 13:14:03 -0600

    Ejercicio 3 Conectando Recycler View

--- Ejercicios/TOY/Ejercicio3_201404106/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Ejercicios/TOY/Ejercicio3_201404106/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -17,11 +17,16 @@ package com.example.android.recyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
-
+private static final int NUM_LIST_ITEMS =100;
+    private GreenAdapter mAdapter;
+    private RecyclerView mNumberList;
     // TODO (2) Create a GreenAdapter variable called mAdapter
     // TODO (3) Create a RecyclerView variable called mNumbersList
 
@@ -29,7 +34,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            mNumberList = (RecyclerView) findViewById(R.id.rv_numbers);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mNumberList.setLayoutManager(layoutManager);
+        mNumberList.setHasFixedSize(true);
 
+        mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+        mNumberList.setAdapter(mAdapter);
         // TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
 
         // TODO (5) Create a LinearLayoutManager variable called layoutManager
